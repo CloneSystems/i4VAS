69b473a95408d0506b0d6d64b2dde175a5710708
Chris <cleesmith2006@gmail.com>
2011-06-17 20:35:29 -0400

    work on Notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -15,12 +15,78 @@ class NotesController < ApplicationController
 
   def new
     @note = Note.new
+    @note.persisted = false
+    @note.report_id = params[:report_id]
+    @note.task_id = params[:task_id]
+    @note.task_name = params[:task_name]
+    @note.result_id = params[:result_id]
+    @note.result_description = params[:result_description]
+    @note.nvt_oid = params[:nvt_oid]
+    @note.hosts = params[:hosts]
+    @note.port = params[:port]
+    @note.threat = params[:threat]
+    # https://192.168.1.4/omp?r=1&cmd=new_note
+    # &result_hosts_only=1
+    # &overrides=1
+    # &notes=1
+    # &hosts=127.0.0.1
+    # &port=general/tcp
+    # &threat=Low
+    # &apply_min_cvss_base=false
+    # &min_cvss_base=
+    # &search_phrase=
+    # &sort_order=descending
+    # &sort_field=type
+    # &levels=hml
+    # &first_result=1
+    # &report_id=f1bc8e10-8e5d-43d3-b2a5-e209546eec9b
+    # &name=3%20at%20once
+    # &task_id=0a2452f0-3aba-496c-9302-b62d9abcaeb4
+    # &oid=1.3.6.1.4.1.25623.1.0.102002
+    # &result_id=de7be789-036b-424c-8f60-2ec200891182
+    # 
+    # new Note with no OID:
+    # https://192.168.1.4/omp?cmd=new_note
+    # &result_id=751d2dd4-4cf1-4d46-b9c9-8d4b0afac336
+    # &oid=0
+    # &task_id=5e67b1e9-2a72-4b6c-b62b-7af71aefeea4
+    # &name=spudder
+    # &report_id=de557f48-9e10-4c13-ba67-60ab2ee83b33
+    # &first_result=1
+    # &levels=g
+    # &sort_field=type
+    # &sort_order=descending
+    # &search_phrase=
+    # &min_cvss_base=
+    # &apply_min_cvss_base=false
+    # &threat=Log
+    # &port=otp%20(9390/tcp)
+    # &hosts=127.0.0.1
+    # &notes=1
+    # &overrides=1
+    # &result_hosts_only=1
+    # 
+    # https://192.168.1.4/omp?cmd=edit_note
+    # &note_id=843fb74c-f412-46f8-b30c-923fd241b9ad
+    # &next=get_report
+    # &report_id=de557f48-9e10-4c13-ba67-60ab2ee83b33
+    # &first_result=1
+    # &sort_field=type
+    # &sort_order=descending
+    # &levels=hm
+    # &notes=1
+    # &overrides=1
+    # &result_hosts_only=1
+    # &search_phrase=
+    # &min_cvss_base=
+    # &apply_min_cvss_base=false
   end
 
   def create
     @note = Note.new(params[:note])
-    if @note.save
-      redirect_to @note, :notice => "Successfully created note."
+    @note.persisted = false
+    if @note.save(current_user)
+      redirect_to notes_url, :notice => "Successfully created note."
     else
       render :action => 'new'
     end
@@ -29,10 +95,12 @@ class NotesController < ApplicationController
   def edit
     redirect_to notes_url, :notice => "*** edit is under development ***"
     # @note = Note.find(params[:id], current_user)
+    # @note.persisted = true
   end
 
   def update
     @note = Note.find(params[:id], current_user)
+    @note.persisted = true
     if @note.update_attributes(params[:note])
       redirect_to @note, :notice  => "Successfully updated note."
     else
--- app/models/note.rb
+++ app/models/note.rb
@@ -3,10 +3,34 @@ class Note
   include OpenvasModel
 
   attr_accessor :nvt_oid, :nvt_name, :hosts, :port, :threat, :note_text, :text_excerpt, :orphan
+  attr_accessor :creation_time, :modification_time
+  attr_accessor :task_id, :task_name
+  attr_accessor :result_id, :result_description
+  attr_accessor :report_id
 
-  define_attribute_methods [:nvt_oid, :nvt_name, :hosts, :port, :threat, :note_text, :text_excerpt, :orphan]
+  define_attribute_methods [:nvt_oid, :nvt_name, :hosts, :port, :threat, :note_text, :task_id, :result_id, :report_id]
 
-  validates :note_text, :length => { :maximum => 600 }
+  validates :note_text, :presence => true, :length => { :maximum => 600 }
+
+  class Selection
+    attr_accessor :id, :name
+    def initialize(attributes = {})
+      attributes.each { |name, value| send("#{name}=", value) } unless attributes.nil?
+    end
+  end
+
+  def self.two_selections(second_id, second_name)
+    s = []
+    s << Selection.new({:id=>'', :name=>'Any'})
+    unless second_id.blank?
+      if second_name.blank?
+        s << Selection.new({:id=>second_id, :name=>second_id})
+      else
+        s << Selection.new({:id=>second_id, :name=>second_name})
+      end
+    end
+    s
+  end
 
   def self.debug_response(user, options = {})
     params = {:details => "1"}
@@ -22,6 +46,7 @@ class Note
   end
 
   # xml response for get single Note request:
+  # 1.
   # <get_notes_response status="200" status_text="OK">
   #   <note id="49e37821-bcca-45b6-ab97-bb8e9a8c09e7">
   #     <nvt oid="1.3.6.1.4.1.25623.1.0.900239">
@@ -53,6 +78,26 @@ class Note
   #     </result>
   #   </note>
   # </get_notes_response>
+  #
+  # 2.
+  # <get_notes_response status="200" status_text="OK">
+  #   <note id="835f7a1e-a313-437c-9636-08bcbea25d16">
+  #     <nvt oid="0">
+  #       <name>(null)</name>
+  #     </nvt>
+  #     <creation_time>Fri Jun 17 19:59:57 2011</creation_time>
+  #     <modification_time>Fri Jun 17 19:59:57 2011</modification_time>
+  #     <text>task: any result: any</text>
+  #     <hosts>127.0.0.1</hosts>
+  #     <port>https (443/tcp)</port>
+  #     <threat>Log</threat>
+  #     <task id="">
+  #       <name/>
+  #     </task>
+  #     <orphan>0</orphan>
+  #     <result id=""/>
+  #   </note>
+  # </get_notes_response>
 
   def self.all(user, options = {})
     params = {:details => "1", :result => "1"}
@@ -61,21 +106,108 @@ class Note
     ret = []
     begin
       resp = user.openvas_connection.sendrecv(req.doc)
-      # Rails.logger.info "\n\n resp=#{resp.to_xml.to_yaml}\n\n"
+      # Rails.logger.info "\n\n Note.all >>> resp=#{resp.to_xml.to_yaml}\n\n"
       resp.xpath("/get_notes_response/note").each { |xml|
         nt = Note.new
-        nt.id           = extract_value_from("@id", xml)
-        nt.nvt_oid      = extract_value_from("nvt/@oid", xml)
-        nt.nvt_name     = extract_value_from("nvt/name", xml)
-        nt.note_text    = extract_value_from("text", xml)
-        nt.text_excerpt = extract_value_from("text/@excerpt", xml)
-        nt.orphan       = extract_value_from("orphan", xml)
+        nt.id                 = extract_value_from("@id", xml)
+        nt.nvt_oid            = extract_value_from("nvt/@oid", xml)
+        nt.nvt_name           = extract_value_from("nvt/name", xml)
+        nt.hosts              = extract_value_from("hosts", xml)
+        nt.hosts              = 'Any' if nt.hosts.blank?
+        nt.port               = extract_value_from("port", xml)
+        nt.port               = 'Any' if nt.port.blank?
+        nt.threat             = extract_value_from("threat", xml)
+        nt.threat             = 'Any' if nt.threat.blank?
+        nt.note_text          = extract_value_from("text", xml)
+        nt.text_excerpt       = extract_value_from("text/@excerpt", xml)
+        nt.orphan             = extract_value_from("orphan", xml)
+        nt.task_id            = extract_value_from("task/@id", xml)
+        nt.task_name          = extract_value_from("task/name", xml)
+        nt.task_name          = 'Any' if nt.task_id.blank?
+        nt.result_id          = extract_value_from("result/@id", xml)
+        nt.result_description = extract_value_from("result/description", xml)
+        nt.result_description = 'Any' if nt.result_id.blank?
+        nt.creation_time      = extract_value_from("creation_time", xml)
+        nt.modification_time  = extract_value_from("modification_time", xml)
         ret << nt
       }
     rescue Exception => e
       raise e
     end
     ret
+  end
+
+  def save(user)
+    if valid?
+      n = Note.find(self.id, user) unless self.id.blank? # for update action
+      n = Note.new if n.blank? # for create action
+      n.name      = self.name
+      n.create_or_update(user)
+      n.errors.each do |attribute, msg|
+        self.errors.add(:openvas, "<br />" + msg)
+      end
+      return false unless self.errors.blank?
+      return true
+    else
+      return false
+    end
+  end
+
+  def update_attributes(user, attrs={})
+    attrs.each { |key, value| send("#{key}=".to_sym, value) if public_methods.include?("#{key}=".to_sym) }
+    save(user)
+  end
+
+  def create_or_update(user)
+    req = Nokogiri::XML::Builder.new { |xml|
+      if @id
+        xml.modify_note(:note_id => @id) {
+          xml.name      { xml.text(@name) }
+          xml.comment   { xml.text(@comment) }
+          xml.login     { xml.text(@login) }    unless @password_type.downcase == 'gen'
+          xml.password  { xml.text(@password) } unless @password_type.downcase == 'gen'
+        }
+      else
+        xml.create_note {
+          xml.name      { xml.text(@name) }
+          xml.comment   { xml.text(@comment) }  unless @comment.blank?
+          xml.login     { xml.text(@login) }    unless @login.blank?
+          xml.password  { xml.text(@password) } unless @password.blank?
+          # nvt_oid= (hidden)
+          # hosts=
+          # port=
+          # threat=
+          # task_id=
+          # result_id=
+          # text=
+        }
+      end
+    }
+    begin
+      # Rails.logger.info "\n\n req.doc=#{req.doc.to_xml.to_yaml}\n\n"
+      resp = user.openvas_connection.sendrecv(req.doc)
+      # Rails.logger.info "\n\n resp=#{resp.to_xml.to_yaml}\n\n"
+      unless Credential.extract_value_from("//@status", resp) =~ /20\d/
+        msg = Credential.extract_value_from("//@status_text", resp)
+        errors[:command_failure] << msg
+        return nil
+      end
+      true
+    rescue Exception => e
+      errors[:command_failure] << e.message
+      nil
+    end
+  end
+
+  def delete_record(user)
+    req = Nokogiri::XML::Builder.new { |xml| xml.delete_note(:note_id => @id) }
+    begin
+      user.openvas_connection.sendrecv(req.doc)
+      true
+    rescue Exception => e
+      errors[:command_failure] << e.message
+      nil
+    end
   end
 
 end
